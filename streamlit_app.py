94f76cd3f1c6d6cc041fc6ade09213036526d92b
Dicky Mohanty <111914281+DickyMohanty@users.noreply.github.com>
2022-08-23 20:01:17 +0000

    Update streamlit_app.py
    Corrected fruit_macros.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ streamlit.text('Curd')
 streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit.macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
