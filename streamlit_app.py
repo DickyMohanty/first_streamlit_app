2d2e558b34ba5d52464e43e05fddea424ea3a6df
Dicky Mohanty <111914281+DickyMohanty@users.noreply.github.com>
2022-08-23 19:59:55 +0000

    Update streamlit_app.py
    Added Fruit List from S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,7 @@ streamlit.text('Curd')
 
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit.macros.txt")
+streamlit.dataframe(my_fruit_list)
