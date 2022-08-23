047a6b3d388f4ff755b5bab307588371fe114794
Dicky Mohanty <111914281+DickyMohanty@users.noreply.github.com>
2022-08-23 19:43:36 +0000

    Update streamlit_app.py
    Added Recipe of Biryani

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('I Love Biryani')
+
+streamlit.header('Recipe')
+streamlit.text('Rice')
+streamlit.text('Meat')
+streamlit.text('Masala')
+streamlit.text('Onion')
+streamlit.text('Ghee')
+streamlit.text('Curd')
