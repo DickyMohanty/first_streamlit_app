6a67b974d1c5809a4272188d44f26db35d9ff22d
Dicky Mohanty <111914281+DickyMohanty@users.noreply.github.com>
2022-08-24 19:08:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ streamlit.header('Build Your Own Fruit Smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# pick the fruit
+steamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
